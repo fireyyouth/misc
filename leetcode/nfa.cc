c097d1922386371cd7fc139873ad37f1f01e23c9
fireyyouth <fireyyouth@gmail.com>
2024-07-01 12:14:33 +0800

    optimize: leetcode nfa

--- leetcode/nfa.cc
+++ leetcode/nfa.cc
@@ -4,16 +4,17 @@
 
 struct Bits {
     vector<uint64_t> data;
+    Bits() {}
     Bits(int n) {
         data.resize((n + 63) / 64);
     }
-    
+
     void set(int i) {
-        data[i / 64] |= 1 << (i % 64);
+        data[i / 64] |= 1ULL<< (i % 64);
     }
 
     bool get(int i) const {
-        return data[i / 64] & (1 << (i % 64));
+        return data[i / 64] & (1ULL<< (i % 64));
     }
 
     void fn_or(const Bits &other) {
@@ -21,66 +22,94 @@ struct Bits {
             data[i] |= other.data[i];
         }
     }
-};
 
-struct NFA {
-    vector<vector<vector<int>>> table;
-    vector<Bits> eps;
+    bool all_zero() const {
+        for (int x: data) {
+            if (x) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-    NFA(string &p) {
-        vector<int> eps_state;
-        table.emplace_back(vector<vector<int>>(26)); // init state
-        int i = 0;
-        while (i < p.size()) {
-            table.emplace_back(vector<vector<int>>(26));
-            if (isalpha(p[i])) {
-                if (i + 1 < p.size() && p[i + 1] == '*') {
-                    table[table.size() - 2][p[i] - 'a'].push_back(table.size() - 2);
-                    eps_state.push_back(table.size() - 2);
-                    i += 2;
-                } else {
-                    table[table.size() - 2][p[i] - 'a'].push_back(table.size() - 1);
-                    i += 1;
-                }
-            } else {
-                if (i + 1 < p.size() && p[i + 1] == '*') {
-                    for (int j = 0; j < 26; ++j) {
-                        table[table.size() - 2][j].push_back(table.size() - 2);
-                    }
-                    eps_state.push_back(table.size() - 2);
-                    i += 2;
-                } else {
-                    for (int j = 0; j < 26; ++j) {
-                        table[table.size() - 2][j].push_back(table.size() - 1);
-                    }
-                    i += 1;
-                }
-            }
-            eps.assign(table.size(), Bits(table.size()));
-            for (int i = 0; i < table.size(); ++i) {
-                eps[i].set(i);
-            }
-            for (int i = (int)eps_state.size() - 1; i >= 0; --i) {
-                eps[eps_state[i]].fn_or(eps[eps_state[i] + 1]);
+    void foreach(function<void(int)> call) const {
+        for (int i = 0; i < data.size(); ++i) {
+            auto t = data[i];
+            while (t) {
+                int j = __builtin_ctz(t);
+                call(i * 64 + j);
+                t &= ~(1ULL << j);
             }
         }
     }
+};
 
-    bool match(string &s) {
-        Bits state(table.size());
-        state.fn_or(eps[0]);
-        for (char ch: s) {
-            Bits next(table.size());
-            bool valid = false;
-            for (int i = 0; i < table.size(); ++i) {
-                if (state.get(i)) {
-                    for (int x: table[i][ch - 'a']) {
-                        next.fn_or(eps[x]);
-                        valid = true;
+struct NFA {
+    vector<vector<Bits>> table;
+    Bits init_eps;
+
+    NFA(string &p) {
+        vector<string_view> seg;
+        {
+            int i = 0;
+            while (i < p.size()) {
+                if (i + 1 < p.size() && p[i + 1] == '*') {
+                    seg.emplace_back(&p[i], 2);
+                    i += 2;
+                } else {
+                    seg.emplace_back(&p[i], 1);
+                    i += 1;
+                }
+            }
+        }
+
+        vector<Bits> eps;
+        {
+            eps.assign(seg.size() + 1, Bits(seg.size() + 1));
+            for (int i = 0; i < eps.size(); ++i) {
+                eps[i].set(i);
+            }
+            for (int i = (int)seg.size() - 1; i >= 0; --i) {
+                if (seg[i].size() == 2) {
+                    eps[i].fn_or(eps[i + 1]);
+                }
+            }
+        }
+
+        table.assign(seg.size() + 1, vector<Bits>(26, Bits(seg.size() + 1)));
+        for (int i = 0; i < seg.size(); ++i) {
+            char ch = seg[i][0];
+            bool is_star = seg[i].size() == 2;
+            if (isalpha(ch)) {
+                if (is_star) {
+                    table[i][ch - 'a'].fn_or(eps[i]);
+                } else {
+                    table[i][ch - 'a'].fn_or(eps[i + 1]);
+                }
+            } else {
+                if (is_star) {
+                    for (int j = 0; j < 26; ++j) {
+                        table[i][j].fn_or(eps[i]);
+                    }
+                } else {
+                    for (int j = 0; j < 26; ++j) {
+                        table[i][j].fn_or(eps[i + 1]);
                     }
                 }
             }
-            if (!valid) {
+        }
+
+        init_eps = eps[0];
+    }
+
+    bool match(string &s) {
+        auto state = init_eps;
+        for (char ch: s) {
+            Bits next(table.size());
+            state.foreach([ch, &next, &table=this->table](int i) {
+                next.fn_or(table[i][ch - 'a']);
+            });
+            if (next.all_zero()) {
                 return false;
             }
             swap(next, state);
