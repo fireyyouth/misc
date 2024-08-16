4d06f60e36a43bd95c7df0da0ba2eb240a1f58d3
fireyyouth <fireyyouth@gmail.com>
2024-08-16 06:16:55 +0000

    chore: twitter elastic scroll

--- vue_demo/twitter/backend.py
+++ vue_demo/twitter/backend.py
@@ -18,15 +18,22 @@ app = FastAPI()
 
 
 @app.get('/preview_list')
-async def preview_list():
+async def preview_list(scroll_id: str = ''):
     es_client = es.get_es()
-    es_result = await es_client.search(index=es.ES_INDEX_TWITTER_POST)
+    if scroll_id:
+        es_result = await es_client.scroll(scroll_id=scroll_id, scroll='1m')
+    else:
+        es_result = await es_client.search(index=es.ES_INDEX_TWITTER_POST, size=3, scroll='1m')
     r = []
     for hit in es_result['hits']['hits']:
         source = hit['_source']
         source['text'] = source['text'][:200] + '...'
         r.append(source)
-    return r
+    print(r)
+    return {
+        'items': r,
+        'scroll_id': es_result['_scroll_id']
+    }
 
 @app.get('/content/{hash}')
 async def content(hash):
