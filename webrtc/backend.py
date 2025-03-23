3deaf7d7b039fc93c5bdcdbbe6dc0e1f04ecf1ec
fireyyouth <fireyyouth@gmail.com>
2025-03-24 00:16:25 +0800

    Update webrtc/backend.py

--- webrtc/backend.py
+++ webrtc/backend.py
@@ -1,28 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+import redis.asyncio as redis
 
 app = FastAPI()
 
-id_list = set()
+client = redis.Redis()
 
-@app.get('/api/')
-def index():
-    return list(id_list)
+async def room_members(room):
+    cursor = 0
+    prefix = 'chat:{}:'.format(room)
+    keys = []
+
+    while True:
+        cursor, partial_keys = await client.scan(cursor, match=prefix + '*')
+        keys.extend(partial_keys)
+        if cursor == 0:  # End of iteration
+            break
+    return [k[len(prefix):] for k in keys]
 
 class PostModel(BaseModel):
     id: str
 
-@app.post('/api/')
-def register(data: PostModel):
-    id_list.add(data.id)
-    return list(id_list)
-
-@app.delete('/api/')
-def register(data: PostModel):
-    id_list.remove(data.id)
-    return list(id_list)
-
+@app.post('/api/{room}')
+async def register(room, data: PostModel):
+    key = 'chat:{}:{}'.format(room, data.id)
+    await client.set(key, '', ex=5)
+    return await room_members(room);
 
 
 uvicorn.run(app, port=5000, log_level="debug")
